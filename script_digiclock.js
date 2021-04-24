59699a09ebbe3528ca7e27c9155bec5596b30c83
Het Modi <71073587+say-het@users.noreply.github.com>
2021-04-24 12:09:50 +0530

    Update script_digiclock.js

--- script_digiclock.js
+++ script_digiclock.js
@@ -10,63 +10,67 @@ function getTime(argument) {
   let hour = d.getHours();
   let min = d.getMinutes();
   let sec = d.getSeconds();
-  // let millsec = d.getMilliseconds();
-  let ampm = hour >= 12 ?'PM':'AM';
-  if (hour>12){
-    hour -= 12; 
+  let ampm = hour >= 12 ?'PM': 'AM';
+  if (hour > 12) {
+    hour -= 12;
+  }
+  if (hour == 0) {
+    hour = 12
   }
   let fullTime = `${hour} : ${min} : ${sec}  ${ampm}`;
-  if (hour < 12 && ampm == 'AM') {
+  if (hour < 12 && hour >= 6 && ampm == 'AM') {
     msg.innerHTML = `Good Morning ${userName}!`
   }
-  if(hour == 12 && ampm == 'PM'){
-    msg.innerHTML = `Good Noon ${userName}!`
-  }
-  if(hour < 3 && ampm == 'PM'){
-    msg.innerHTML = `Good Noon ${userName}!`
-  }
-  if(hour == 3 && ampm == 'PM'){
-    msg.innerHTML = `Good Afternoon ${userName}!`
-  }
-  if(hour < 5 && hour > 3 && ampm == 'PM'){
-    msg.innerHTML = `Good Afternoon ${userName}!`
-  }
-  if(hour == 5 && ampm == 'PM'){
-    msg.innerHTML = `Good Evening ${userName}!`
-  }
-  if(hour < 8 && hour > 5 && ampm == 'PM'){
-    msg.innerHTML = `Good Evening ${userName}!`
-  }
-  if(hour == 8 && ampm == 'PM'){
+  if (hour < 6 && hour > 1 && ampm == 'AM') {
     msg.innerHTML = `Good Night ${userName}!`
   }
-  if(hour < 12 && hour > 8 && ampm == 'PM'){
+  if (hour == 12 && ampm == 'PM') {
+    msg.innerHTML = `Good Noon ${userName}!`
+  }
+  if (hour == 12 && ampm == 'AM') {
+    msg.innerHTML = `Good Night ${userName}!`
+  }
+  if (hour < 3 && ampm == 'PM') {
+    msg.innerHTML = `Good Noon ${userName}!`
+  }
+  if (hour == 3 && ampm == 'PM') {
+    msg.innerHTML = `Good Afternoon ${userName}!`
+  }
+  if (hour < 5 && hour > 3 && ampm == 'PM') {
+    msg.innerHTML = `Good Afternoon ${userName}!`
+  }
+  if (hour == 5 && ampm == 'PM') {
+    msg.innerHTML = `Good Evening ${userName}!`
+  }
+  if (hour < 8 && hour > 5 && ampm == 'PM') {
+    msg.innerHTML = `Good Evening ${userName}!`
+  }
+  if (hour == 8 && ampm == 'PM') {
+    msg.innerHTML = `Good Night ${userName}!`
+  }
+  if (hour < 12 && hour > 8 && ampm == 'PM') {
     msg.innerHTML = `Good Night ${userName}!`
   }
   time.innerHTML = fullTime;
 }
-let date = new Date();
-let hours = date.getHours();
-let ampms = hours >= 12 ?'PM':'AM';
-if (hours>12){
-    hours -= 12; 
-}
-console.log(hours)
-if (hours >= 8 && ampms == 'PM') {
-    img.src = 'https://say-het.github.io/morning.jpeg'    
-}
-if (hours >= 5 && hours < 8 && ampms == 'PM') {
-    img.src = 'https://say-het.github.io/evening.jpeg'    
-}
-if (hours >= 7  && hours < 12 && ampms == 'AM') {
-    img.src = 'https://say-het.github.io/morning.jpeg'    
-}
-if (hours == 12 && ampms == 'PM') {
-    img.src = 'https://say-het.github.io/afternoon.jpeg'    
-}
-if (hours < 5 && ampms == 'PM') {
-    img.src = 'https://say-het.github.io/afternoon.jpeg'    
-}
 
-getTime();
-setInterval(getTime, 1000);
+function setWallpaper(argument) {
+  if (msg.innerHTML == `Good Morning ${userName}!`) {
+    img.src = 'http://localhost:8158/Images/morning.jpeg'
+  }
+  if (msg.innerHTML == `Good Noon ${userName}!`) {
+    img.src = 'http://localhost:8158/Images/afternoon.jpeg'
+  }
+  if (msg.innerHTML == `Good Afternoon ${userName}!`) {
+    img.src = 'http://localhost:8158/Images/afternoon.jpeg'
+  }
+  if (msg.innerHTML == `Good Evening ${userName}!`) {
+    img.src = 'http://localhost:8158/Images/evening.jpeg'
+  }
+  if (msg.innerHTML == `Good Night ${userName}!`) {
+    img.src = 'http://localhost:8158/Images/night.jpeg'
+  }
+  }
+  setTimeout(setWallpaper, 100);
+  getTime();
+  setInterval(getTime, 1000);
