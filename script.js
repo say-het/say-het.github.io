ceca0d6dc411aca60b2e5c88a0cac4c1103cc200
Het Modi <71073587+say-het@users.noreply.github.com>
2021-05-05 12:57:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,102 +1,33 @@
-let imgSec = document.getElementById('imgSec');
+// Container where search bar and search button is
 let searchContainer = document.getElementById('searchContainer');
-let searchBtn = document.getElementById('searchBtn');
-let app_id = `5efbbf7a`;
-let api_key = `acfa77210b5c437783a623bad6232aa1`;
-console.log(searchBox.value);
-searchBtn.addEventListener('click', fetchData)
-function fetchData(e) {
-  e.preventDefault();
-  fetch(`https://api.edamam.com/search?app_id=5efbbf7a&app_key=acfa77210b5c437783a623bad6232aa1&q=${searchBox.value}`).then(response => response.json()).then((data)=> {
-    console.log(data);
-    console.log(imgSec);
-    console.log(searchBox.value);
 
-    let html = ``;
+localStorage.clear();
+let T = `Any Countries' Any Information`;
+let D = `You can find any Countries' Any information like It's Name,Population,Area,Capital,Calling Code etc..`;
+let L = `anyInfoAboutCountry_index.html`;
+localStorage.setItem('T',T)
+localStorage.setItem('D',D)
+localStorage.setItem('L',L)
+// This is where projects are listed
+let projectsList = document.getElementById('projectsList');
+    html = `<div id="accordion">
+    <div class="card">
+    <div class="card-header" id="headingOne">
+    <h5 class="mb-0">
+    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+  ${localStorage.getItem('T')}
+    </button>
+    </h5>
+    </div>
 
-    
-    let img0 = data.hits[0].recipe.image;
-    let recipeName0 = data.hits[0].recipe.label;
-      
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+    <div class="card-body">
+  ${localStorage.getItem('D')}
+     <a href=${localStorage.getItem('L')}>Go To Project...</a>
+    </div>
+    </div>
+    </div>
 
-
-    html += `<img src="${img0}" alt="" /><h6>${recipeName0}</h6><h6>${data.hits[0].recipe.ingredientLines[0]}<br />${data.hits[0].recipe.ingredientLines[1]}<br />${data.hits[0].recipe.ingredientLines[2]}<br />${data.hits[0].recipe.ingredientLines[3]}<br />${data.hits[0].recipe.ingredientLines[4]}<br />${data.hits[0].recipe.ingredientLines[5]}<br />${data.hits[0].recipe.ingredientLines[6]}<br />${data.hits[0].recipe.ingredientLines[7]}<br />${data.hits[0].recipe.ingredientLines[8]}<br />${data.hits[0].recipe.ingredientLines[9]}<br />${data.hits[0].recipe.ingredientLines[10]}<br />${data.hits[0].recipe.ingredientLines[11]}<br />${data.hits[0].recipe.ingredientLines[12]}<br />${data.hits[0].recipe.ingredientLines[13]}<br />${data.hits[0].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img1 = data.hits[1].recipe.image;
-    let recipeName1 = data.hits[1].recipe.label;
-    
-
-
-    html += `<img src="${img1}" alt="" /><h6>${recipeName1}</h6><h6>${data.hits[1].recipe.ingredientLines[0]}<br />${data.hits[1].recipe.ingredientLines[1]}<br />${data.hits[1].recipe.ingredientLines[2]}<br />${data.hits[1].recipe.ingredientLines[3]}<br />${data.hits[1].recipe.ingredientLines[4]}<br />${data.hits[1].recipe.ingredientLines[5]}<br />${data.hits[1].recipe.ingredientLines[6]}<br />${data.hits[1].recipe.ingredientLines[7]}<br />${data.hits[1].recipe.ingredientLines[8]}<br />${data.hits[1].recipe.ingredientLines[9]}<br />${data.hits[1].recipe.ingredientLines[10]}<br />${data.hits[1].recipe.ingredientLines[11]}<br />${data.hits[1].recipe.ingredientLines[12]}<br />${data.hits[1].recipe.ingredientLines[13]}<br />${data.hits[1].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img2 = data.hits[2].recipe.image;
-    let recipeName2 = data.hits[2].recipe.label;
-    
-
-
-    html += `<img src="${img2}" alt="" /><h6>${recipeName2}</h6><h6>${data.hits[2].recipe.ingredientLines[0]}<br />${data.hits[2].recipe.ingredientLines[1]}<br />${data.hits[2].recipe.ingredientLines[2]}<br />${data.hits[2].recipe.ingredientLines[3]}<br />${data.hits[2].recipe.ingredientLines[4]}<br />${data.hits[2].recipe.ingredientLines[5]}<br />${data.hits[2].recipe.ingredientLines[6]}<br />${data.hits[2].recipe.ingredientLines[7]}<br />${data.hits[2].recipe.ingredientLines[8]}<br />${data.hits[2].recipe.ingredientLines[9]}<br />${data.hits[2].recipe.ingredientLines[10]}<br />${data.hits[2].recipe.ingredientLines[11]}<br />${data.hits[2].recipe.ingredientLines[12]}<br />${data.hits[2].recipe.ingredientLines[13]}<br />${data.hits[2].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img3 = data.hits[3].recipe.image;
-    let recipeName3 = data.hits[3].recipe.label;
-    
-
-
-    html += `<img src="${img3}" alt="" /><h6>${recipeName3}</h6><h6>${data.hits[3].recipe.ingredientLines[0]}<br />${data.hits[3].recipe.ingredientLines[1]}<br />${data.hits[3].recipe.ingredientLines[2]}<br />${data.hits[3].recipe.ingredientLines[3]}<br />${data.hits[3].recipe.ingredientLines[4]}<br />${data.hits[3].recipe.ingredientLines[5]}<br />${data.hits[3].recipe.ingredientLines[6]}<br />${data.hits[3].recipe.ingredientLines[7]}<br />${data.hits[3].recipe.ingredientLines[8]}<br />${data.hits[3].recipe.ingredientLines[9]}<br />${data.hits[3].recipe.ingredientLines[10]}<br />${data.hits[3].recipe.ingredientLines[11]}<br />${data.hits[3].recipe.ingredientLines[12]}<br />${data.hits[3].recipe.ingredientLines[13]}<br />${data.hits[3].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img4 = data.hits[4].recipe.image;
-    let recipeName4 = data.hits[4].recipe.label;
-    
-
-
-    html += `<img src="${img4}" alt="" /><h6>${recipeName4}</h6><h6>${data.hits[4].recipe.ingredientLines[0]}<br />${data.hits[4].recipe.ingredientLines[1]}<br />${data.hits[4].recipe.ingredientLines[2]}<br />${data.hits[4].recipe.ingredientLines[3]}<br />${data.hits[4].recipe.ingredientLines[4]}<br />${data.hits[4].recipe.ingredientLines[5]}<br />${data.hits[4].recipe.ingredientLines[6]}<br />${data.hits[4].recipe.ingredientLines[7]}<br />${data.hits[4].recipe.ingredientLines[8]}<br />${data.hits[4].recipe.ingredientLines[9]}<br />${data.hits[4].recipe.ingredientLines[10]}<br />${data.hits[4].recipe.ingredientLines[11]}<br />${data.hits[4].recipe.ingredientLines[12]}<br />${data.hits[4].recipe.ingredientLines[13]}<br />${data.hits[4].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img5 = data.hits[5].recipe.image;
-    let recipeName5 = data.hits[5].recipe.label;
-    
-
-
-    html += `<img src="${img5}" alt="" /><h6>${recipeName5}</h6><h6>${data.hits[5].recipe.ingredientLines[0]}<br />${data.hits[5].recipe.ingredientLines[1]}<br />${data.hits[5].recipe.ingredientLines[2]}<br />${data.hits[5].recipe.ingredientLines[3]}<br />${data.hits[5].recipe.ingredientLines[4]}<br />${data.hits[5].recipe.ingredientLines[5]}<br />${data.hits[5].recipe.ingredientLines[6]}<br />${data.hits[5].recipe.ingredientLines[7]}<br />${data.hits[5].recipe.ingredientLines[8]}<br />${data.hits[5].recipe.ingredientLines[9]}<br />${data.hits[5].recipe.ingredientLines[10]}<br />${data.hits[5].recipe.ingredientLines[11]}<br />${data.hits[5].recipe.ingredientLines[12]}<br />${data.hits[5].recipe.ingredientLines[13]}<br />${data.hits[5].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img6 = data.hits[6].recipe.image;
-    let recipeName6 = data.hits[6].recipe.label;
-    
-
-
-    html += `<img src="${img6}" alt="" /><h6>${recipeName6}</h6><h6>${data.hits[6].recipe.ingredientLines[0]}<br />${data.hits[6].recipe.ingredientLines[1]}<br />${data.hits[0].recipe.ingredientLines[2]}<br />${data.hits[6].recipe.ingredientLines[3]}<br />${data.hits[6].recipe.ingredientLines[4]}<br />${data.hits[6].recipe.ingredientLines[5]}<br />${data.hits[6].recipe.ingredientLines[6]}<br />${data.hits[6].recipe.ingredientLines[7]}<br />${data.hits[6].recipe.ingredientLines[8]}<br />${data.hits[6].recipe.ingredientLines[9]}<br />${data.hits[6].recipe.ingredientLines[10]}<br />${data.hits[6].recipe.ingredientLines[11]}<br />${data.hits[6].recipe.ingredientLines[12]}<br />${data.hits[6].recipe.ingredientLines[13]}<br />${data.hits[6].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img7 = data.hits[7].recipe.image;
-    let recipeName7 = data.hits[7].recipe.label;
-    
-
-
-    html += `<img src="${img7}" alt="" /><h6>${recipeName7}</h6><h6>${data.hits[7].recipe.ingredientLines[0]}<br />${data.hits[7].recipe.ingredientLines[1]}<br />${data.hits[7].recipe.ingredientLines[2]}<br />${data.hits[7].recipe.ingredientLines[3]}<br />${data.hits[7].recipe.ingredientLines[4]}<br />${data.hits[7].recipe.ingredientLines[5]}<br />${data.hits[7].recipe.ingredientLines[6]}<br />${data.hits[7].recipe.ingredientLines[7]}<br />${data.hits[7].recipe.ingredientLines[8]}<br />${data.hits[7].recipe.ingredientLines[9]}<br />${data.hits[7].recipe.ingredientLines[10]}<br />${data.hits[7].recipe.ingredientLines[11]}<br />${data.hits[7].recipe.ingredientLines[12]}<br />${data.hits[7].recipe.ingredientLines[13]}<br />${data.hits[7].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img8 = data.hits[8].recipe.image;
-    let recipeName8 = data.hits[8].recipe.label;
-    
-
-
-    html += `<img src="${img8}" alt="" /><h6>${recipeName8}</h6><h6>${data.hits[8].recipe.ingredientLines[0]}<br />${data.hits[8].recipe.ingredientLines[1]}<br />${data.hits[8].recipe.ingredientLines[2]}<br />${data.hits[8].recipe.ingredientLines[3]}<br />${data.hits[8].recipe.ingredientLines[4]}<br />${data.hits[8].recipe.ingredientLines[5]}<br />${data.hits[8].recipe.ingredientLines[6]}<br />${data.hits[8].recipe.ingredientLines[7]}<br />${data.hits[8].recipe.ingredientLines[8]}<br />${data.hits[8].recipe.ingredientLines[9]}<br />${data.hits[8].recipe.ingredientLines[10]}<br />${data.hits[8].recipe.ingredientLines[11]}<br />${data.hits[8].recipe.ingredientLines[12]}<br />${data.hits[8].recipe.ingredientLines[13]}<br />${data.hits[8].recipe.ingredientLines[14]}</h6><br>`
-    
-    let img9 = data.hits[9].recipe.image;
-    let recipeName9 = data.hits[9].recipe.label;
-    
-
-
-    html += `<img src="${img9}" alt="" /><h6>${recipeName9}</h6><h6>${data.hits[9].recipe.ingredientLines[0]}<br />${data.hits[9].recipe.ingredientLines[1]}<br />${data.hits[9].recipe.ingredientLines[2]}<br />${data.hits[9].recipe.ingredientLines[3]}<br />${data.hits[9].recipe.ingredientLines[4]}<br />${data.hits[9].recipe.ingredientLines[5]}<br />${data.hits[9].recipe.ingredientLines[6]}<br />${data.hits[9].recipe.ingredientLines[7]}<br />${data.hits[9].recipe.ingredientLines[8]}<br />${data.hits[9].recipe.ingredientLines[9]}<br />${data.hits[9].recipe.ingredientLines[10]}<br />${data.hits[9].recipe.ingredientLines[11]}<br />${data.hits[9].recipe.ingredientLines[12]}<br />${data.hits[9].recipe.ingredientLines[13]}<br />${data.hits[9].recipe.ingredientLines[14]}</h6><br>`
-console.log(html)
-    imgSec.innerHTML = html;
-  })
-
-
-
-
-}
-$("body").each(function() {
-    var text = $(this).text();
-    text = text.replace("undefined", "");
-    $(this).text(text);
-});
-
-
-// https://api.edamam.com/search?app_id=5efbbf7a&app_key=acfa77210b5c437783a623bad6232aa1&q=pizza
+    </div>`;
+projectsList.innerHTML += html;
+   
