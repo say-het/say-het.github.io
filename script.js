c2af3064b9583e7078c20333afc84782176dfd91
Het Modi <71073587+say-het@users.noreply.github.com>
2021-05-05 13:23:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,45 +3,38 @@ let searchContainer = document.getElementById('searchContainer');
 
 // This is where projects are listed
 let projectsList = document.getElementById('projectsList');
-    html = `<div id="accordion">
-    <div class="card">
+    html = `<div class="accordion" id="accordionExample">
+  <div class="card">
     <div class="card-header" id="headingOne">
-    <h5 class="mb-0">
-    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-  Any Countries' Any Information
-    </button>
-    </h5>
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+          Any Countries' Any Information
+        </button>
+      </h2>
     </div>
 
-    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-    <div class="card-body">
-  You can find any Countries' Any information like It's Name,Population,Area,Capital,Calling Code etc..
-     <a href='anyInfoAboutCountry_index.html'>Go To Project...</a>
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        You can find any Countries' Any information like It's Name,Population,Area,Capital,Calling Code etc..
+        <a href='anyInfoAboutCountry_index.html'>Go To Project...</a>
+      </div>
     </div>
-    </div>
-    </div>
-
-    </div>
-    <div id="accordion">
-    <div class="card">
+  </div>
+  <div class="card">
     <div class="card-header" id="headingTwo">
-    <h5 class="mb-0">
-    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-  Password Generator
-    </button>
-    </h5>
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Password Generator
+        </button>
+      </h2>
     </div>
-
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-    <div class="card-body">
-  It can generate the hardest and strongest password (for your security) based on length you enter. 
-     <a href='passwordGenerator_index.html'>Go To Project...</a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        It can generate the hardest and strongest password (for your security) based on length you enter. <a href='passwordGenerator_index.html'>Go To Project...</a>
+      </div>
     </div>
-    </div>
-    </div>
-
-    </div>
-    
+  </div>
+</div>
     `;
 projectsList.innerHTML += html;
    
