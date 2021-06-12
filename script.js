5aee1a5397bc134b032e0e442d139fd60c2a4fe6
Het Modi <71073587+say-het@users.noreply.github.com>
2021-06-12 18:23:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,21 @@ let projectsList = document.getElementById('projectsList');
       </div>
     </div>
   </div>
+  
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          To Do List
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        You can write down your pending task and edit it delete it after it's completed and even delete the all tasks<a href='todo_index.html'>Go To Project...</a>
+</div>
+    </div>
+  </div>
 </div>
     `;
 projectsList.innerHTML += html;
